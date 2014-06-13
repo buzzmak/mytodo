cfef588d6c2691fa4f644eb4671e3ce42adaa0e3
buzzmak <mkoch4@gmail.com>
2014-06-13 16:46:37 +0200

    adding some functions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,25 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-    .controller('MainCtrl', function ($scope) {
-        $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    .controller('MainCtrl', function ($scope, localStorageService) {
+
+
+        // keep todos persistent:
+        var todosInStore = localStorageService.get('todos');
+
+        $scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+        $scope.$watch('todos', function () {
+            localStorageService.add('todos', $scope.todos.join('\n'));
+        }, true);
+
+        // add todos:
         $scope.addTodo = function () {
             $scope.todos.push($scope.todo);
             $scope.todo = '';
         };
+
+        // store todos:
         $scope.removeTodo = function (index) {
             $scope.todos.splice(index, 1);
         };
