f84e1e10a874a204733c9cd94e5b133349cbe5ce
buzzmak <mkoch4@gmail.com>
2014-06-13 13:27:10 +0200

    added mock dir and altered karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -14,6 +14,12 @@ module.exports = function(config) {
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
 
+    reporters: ['dots', 'junit'],
+
+        junitReporter: {
+            outputFile: 'test-results.xml'
+        },
+
     // list of files / patterns to load in the browser
     files: [
       'bower_components/angular/angular.js',
@@ -55,13 +61,14 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false,
+    singleRun: true,
 
     colors: true,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
+
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
